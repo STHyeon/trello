f14f4eceda07c78ffabc6c924c118bb12839bedf
STHyeon <toby2009@naver.com>
2020-09-01 15:48:57 +0900

    로고 변경

--- src/components/atoms/image/Image.tsx
+++ src/components/atoms/image/Image.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import { CommonProps } from "../../../assets/utils/CommonType";
 
-function Image() {
-    return <div>aa</div>;
+interface ImageProps extends CommonProps {
+    logo?: boolean;
+
+    link: string;
+    alt: string;
+}
+
+const StyledImage = styled.div<ImageProps>`
+    ${(props) =>
+        props.logo &&
+        css`
+            height: 31px;
+
+            img {
+                height: 100%;
+            }
+        `}
+`;
+
+function Image({ ...props }: ImageProps) {
+    const { alt, link } = props;
+    return (
+        <StyledImage {...props}>
+            <img src={link} alt={alt} />
+        </StyledImage>
+    );
 }
 
 export default Image;
--- src/components/molecules/nav/navCenter/NavCenter.tsx
+++ src/components/molecules/nav/navCenter/NavCenter.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import NavBox from "../navBox/NavBox";
-import { Home as HomeIcon } from "@material-ui/icons";
+import { Image } from "../../../atoms";
+
+import LOGO from "../../../../assets/img/logo_w.png";
 
 function NavCenter() {
-    return (
-        <div>
-            <NavBox link="/">
-                <HomeIcon />
-            </NavBox>
-        </div>
-    );
+    return <Image logo link={LOGO} alt="로고" />;
 }
 
 export default NavCenter;
